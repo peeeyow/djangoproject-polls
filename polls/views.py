ff2796a83d2f5a95fae6998bec9c1ad716b215fa
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-05-15 16:25:17 +0800

    Display 5 most recent questions in polls/

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    top5_questions = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join(question.question_text for question in top5_questions)
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
