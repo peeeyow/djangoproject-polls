7a7cf4dc2c5543ece9f1c4c6fefc567f1013adbe
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-05-15 16:58:51 +0800

    Raise Error 404 page if question DNE

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http.response import Http404
 from django.shortcuts import render
 
 from polls.models import Question
@@ -13,7 +14,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exists.")
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, question_id):
